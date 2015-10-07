5afa5a3fd01e99b4757fc33846d610fea473da88
Ruby Schmidmeister <ruby@fog.im>
2015-10-07 20:38:47 +0200

    add copyright, add type declarations

--- src/URL.hs
+++ src/URL.hs
@@ -1,11 +1,19 @@
+--
+-- (c) 2015 Ruben Schmidmeister
+--
+
 module URL where
 
 import Data.Char (ord)
 import Data.List (concat, isInfixOf)
 import Numeric (showHex)
 
+
+reservedChars :: String
 reservedChars = ":/?#[]@!$&()'*+,;= "
 
+
+encodeChar :: Char -> String
 encodeChar char =
     if isInfixOf [char] reservedChars
         then
@@ -13,5 +21,7 @@ encodeChar char =
         else
             [char]
 
+
+encodeString :: String -> String
 encodeString str =
     concat (map encodeChar str)
